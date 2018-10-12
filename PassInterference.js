4f87fcf10b663ab1b5e04c6e5394cd7141b12648
tidalwade <35672042+tidalwade@users.noreply.github.com>
2018-10-12 11:15:11 -0500

    v7

--- PassInterference.js
+++ PassInterference.js
@@ -112,7 +112,10 @@ $('input[placeholder=""]').on("keypress", {
 	// console.log(e.key);
 	var y = x.toUpperCase();
 	console.log(y);
-	$(this).next().trigger("focus");
+	if(jQuery(window).width() > 768) {
+		$(this).next().trigger("focus");
+		currentIndex = (currentIndex + 1)
+	}
 	if(placeHolder == y && IDs == secretKey){
 		// console.log("this is y: " + y);
 		// y == 0;
