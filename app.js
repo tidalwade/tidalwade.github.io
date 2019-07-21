72dc93811b1faf1b019a56849bcc65424de6f025
tidalwade <ryantwade@hotmail.com>
2019-07-21 10:14:25 -0500

    vs code node training course changes

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log('Node is correctly installed');
+const tutorial = require('./tutorial')
+console.log(tutorial);
+console.log(tutorial.sum(1,1));
+console.log(tutorial.PI);
---
+++ tutorial.js
@@ -0,0 +1,4 @@
+const sum = (num1,num2) => num1 + num2;
+const PI = 3.14;
+module.exports.sum = sum;
+module.exports.PI = PI;
